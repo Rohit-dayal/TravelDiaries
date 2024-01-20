d6f91be049454ae33d4ef9757921cc1414db1672
Rohit Dayal <rohitdayal118@gmail.com>
2024-01-21 03:57:43 +0530

    created header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import Signin from './Pages/Signin'
 import SignUp from './Pages/SignUp'
 import Dashboard from './Pages/Dashboard'
 import Home from './Pages/Home'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path ="/" element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
