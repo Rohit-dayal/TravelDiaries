64d2d8ff4acfec444243cdde20b39fb41d893839
Rohit Dayal <rohitdayal118@gmail.com>
2024-01-21 17:41:31 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 // We created index.js file because in package.json the main file is mentioned as index.js
 
 dotenv.config();
@@ -15,9 +16,13 @@ mongoose.connect(
     console.log(err);
 })
 const app = express();
+// As a default we are not allowed to send the json to the backend so we need to use the below line. 
+//This will allow the json as a input to the backend
+app.use(express.json());
 
 app.listen(3000,() => {
     console.log('server is running on port 3000 !!')
 })
 
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
