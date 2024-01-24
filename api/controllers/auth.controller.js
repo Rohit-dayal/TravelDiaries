59048521c6c65ee331b0517c35007150c89b3b2b
Rohit Dayal <rohitdayal118@gmail.com>
2024-01-24 19:35:25 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils.js/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res,next)=>{
     const {username,email,password} = req.body;
@@ -19,11 +20,38 @@ export const signup = async (req,res,next)=>{
 
     try {
         await newUser.save();
-        console.log(newUser)
+        // console.log(newUser)
       res.json('Signup successful')
     } catch (error) {
-        console.log(error)
+        // console.log(error)
+        next(error);
+    }  
+}
+
+export const signin = async (req,res,next) =>{
+    const {email,password} = req.body;
+
+    if(!email ||!password || email === ''  || password === ''){
+        next(errorHandler(400,'All fields are required'))
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,'User not found'));
+        } 
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword){
+            return next(errorHandler(404,'Invalid password'));
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET,)
+        // {expiresIn:'1d'} we use this when we want that the user will be logged out once he closes his browser
+        
+        const {password: pass,...rest} = validUser._doc
+
+        // we set the httpOnly: true to make our cookie secure
+        res.status(200).cookie('access_token',token, {httpOnly: true}).json(rest)
+    } catch (error) {
         next(error);
     }
-    
 }
